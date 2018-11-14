58162a62775f9e4b9774ffe27716ef32b842a7e4
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2018-11-14 08:52:06 +0300

    Initial Commit

---
+++ CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Ильдар Залялов on 14/11/2018.
+//  Copyright © 2018 Ильдар Залялов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
